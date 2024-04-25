da85db163fb1ba67a4f296febb5a3d77a19cc0b2
Tymofii <159185555+tymofii-stepanov@users.noreply.github.com>
2024-04-25 18:27:00 +0000

    Done work

--- js/script.js
+++ js/script.js
@@ -1,20 +1,16 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
 
-/**
- * This function displays the slider value.
- */
-function myButtonClicked() {
-  alwaysOnButtonChecked = document.getElementById('on-check').checked
+function generateRandomNumber() {
+  // Get the selected radio button
+  var positiveChecked = document.getElementById('positive').checked;
 
-  if (alwaysOnButtonChecked == true) {
-    document.getElementById("radio-button-value").innerHTML =
-      "<p>Value is: On</p>"
+  // Generate a random number
+  var randomNumber;
+  if (positiveChecked) {
+    randomNumber = Math.floor(Math.random() * 6) + 1; 
   } else {
-    document.getElementById("radio-button-value").innerHTML =
-      "<p>Value is: Off</p>"
+    randomNumber = -1 * (Math.floor(Math.random() * 6) + 1);
   }
+
+  // Display the random number
+  document.getElementById("randomNumber").innerHTML = "<p>Random Number: " + randomNumber + "</p>";
 }
