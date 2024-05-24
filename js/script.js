9ca3d6fa14eda96e21ffbdf1665ac3f27a2b6f5d
Tymofii <159185555+tymofii-stepanov@users.noreply.github.com>
2024-05-24 18:32:21 +0000

    done

--- js/script.js
+++ js/script.js
@@ -6,19 +6,17 @@
 
 "use strict";
 
-
 function generateRandomNumber() {
-  // Get the selected radio button
-  var positiveChecked = document.getElementById('positive').checked;
+  const positiveChecked = document.getElementById('positive').checked;
 
-  // Generate a random number
+
   let randomNumber;
-  if (positiveChecked) {
+  if (positiveChecked === true) {
     randomNumber = Math.floor(Math.random() * 6) + 1
   } else {
-    randomNumber = -1 * (Math.floor(Math.random() * 6) + 1)
+    randomNumber = -(Math.floor(Math.random() * 6) + 1)
   }
 
-  // Display the random number
-  document.getElementById("randomNumber").innerHTML = "<p>Random Number: " + randomNumber + "</p>"
+
+  document.getElementById("randomNumber").innerHTML = `<p>Random Number: ${randomNumber}</p>`
 }
